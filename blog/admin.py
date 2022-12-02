0ad020326fe3131bfd2c6b9174d32306ab35e8e3
VITOR-HUGO94 <vitorhugobrizante@gmail.com>
2022-12-02 18:04:15 -0300

    hosts

--- blog/admin.py
+++ blog/admin.py
@@ -8,7 +8,7 @@ class AuthorAdmin(admin.ModelAdmin):
     list_display = ('title', 'id', 'status', 'slug', 'author')
     prepopulated_fields = {'slug': ('title',), }
 
-@csrf_protect
+
 admin.site.register(models.Category)
-@csrf_protect
+
 admin.site.register(models.Carousel)
