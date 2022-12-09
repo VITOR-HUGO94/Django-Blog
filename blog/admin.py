e4355a1ab465c6ae89d26ffd25f8cd21c96cb397
VITOR-HUGO94 <vitorhugobrizante@gmail.com>
2022-12-09 15:32:27 -0300

    admin.py csrf

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,11 @@
 from django.contrib import admin
 from . import models
-from django.views.decorators.csrf import csrf_protect
 
-@csrf_protect
 @admin.register(models.Post)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('title', 'id', 'status', 'slug', 'author')
     prepopulated_fields = {'slug': ('title',), }
 
-@csrf_protect
-@admin.site.register(models.Category)
+admin.site.register(models.Category)
 
 admin.site.register(models.Carousel)
