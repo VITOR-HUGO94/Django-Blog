24876e4fc26a8e5fcb7eb024dbb0155d9639cfb3
VITOR-HUGO94 <vitorhugobrizante@gmail.com>
2022-12-09 16:13:51 -0300

    allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,10 +23,10 @@ SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://django-blog-production-e67d.up.railway.app']
+ALLOWED_HOSTS = ['*']
 
 
-CSRF_TRUSTED_ORIGINS = ['http://django-blog-production-e67d.up.railway.app/']
+#CSRF_TRUSTED_ORIGINS = ['http://django-blog--e67d.up.railway.app/']
 #ALLOWED_HOSTS = [
 #    '*',
 #],
