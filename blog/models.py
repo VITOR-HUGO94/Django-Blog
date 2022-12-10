c9bc06a38ee06e3a24d4fb318baefdc496de523b
VITOR-HUGO94 <vitorhugobrizante@gmail.com>
2022-12-09 21:20:22 -0300

    models and index carousels

--- blog/models.py
+++ blog/models.py
@@ -86,9 +86,9 @@ class Comment(MPTTModel):
 
 class Carousel(models.Model):
     Image = models.ImageField(upload_to='Carousel', null = True)
-    #Img_title = models.CharField(max_length=30, null=True)
-    #Img_desc = models.CharField(max_length=500, null=True)
+    Img_title = models.CharField(max_length=30, null=True)
+    Img_desc = models.CharField(max_length=500, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
-        return self.Img_title
+       return self.Img_title
